9f51eabb0c25c7fc4c170881ac02726482caaf43
Rafał Lisak <rafal.lisak@appliscale.io>
2021-04-09 22:08:38 +0200

    init

---
+++ src/main/java/agents/SlaveAgent.java
@@ -0,0 +1,118 @@
+/*****************************************************************
+ JADE - Java Agent DEvelopment Framework is a framework to develop
+ multi-agent systems in compliance with the FIPA specifications.
+ Copyright (C) 2000 CSELT S.p.A.
+
+ GNU Lesser General Public License
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation,
+ version 2.1 of the License.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
+
+package agents;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class SlaveAgent extends Agent {
+    // The title of the book to buy
+    private String targetBookTitle;
+    // The list of known seller agents
+    private AID[] sellerAgents;
+
+    // Put agent initializations here
+    protected void setup() {
+        // Printout a welcome message
+        System.out.println("Hallo! Slave "+getAID().getName()+" is ready.");
+
+        // Get the title of the book to buy as a start-up argument
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            targetBookTitle = (String) args[0];
+            System.out.println("Target book is "+targetBookTitle);
+            // Add a TickerBehaviour that schedules a request to seller agents every minute
+            addBehaviour(new OneShotBehaviour(this) {
+                @Override
+                public void action() {
+                    System.out.println("Trying to buy "+targetBookTitle);
+                    // Update the list of seller agents
+                    DFAgentDescription template = new DFAgentDescription();
+                    ServiceDescription sd = new ServiceDescription();
+                    sd.setType("book-selling");
+                    template.addServices(sd);
+                    try {
+                        DFAgentDescription[] result = DFService.search(myAgent, template);
+                        System.out.println("Found the following seller agents:");
+                        sellerAgents = new AID[result.length];
+                        for (int i = 0; i < result.length; ++i) {
+                            sellerAgents[i] = result[i].getName();
+                            System.out.println(sellerAgents[i].getName());
+                        }
+                    }
+                    catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }
+
+                    // Perform the request
+                    myAgent.addBehaviour(new RequestPerformer());
+                }
+            } );
+        }
+    }
+
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        // Printout a dismissal message
+        System.out.println("Buyer-agent "+getAID().getName()+" terminating.");
+    }
+
+    /**
+     Inner class RequestPerformer.
+     This is the behaviour used by Book-buyer agents to request seller
+     agents the target book.
+     */
+    private class RequestPerformer extends Behaviour {
+
+        public void action() {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+                    // Send the cfp to all sellers
+                    ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+                    for (int i = 0; i < sellerAgents.length; ++i) {
+                        cfp.addReceiver(sellerAgents[i]);
+                    }
+                    cfp.setContent("done");
+                    cfp.setConversationId("book-trade");
+                    cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+                    myAgent.send(cfp);
+        }
+
+        public boolean done() {
+            System.out.println("Done");
+           return true;
+        }
+    }
+}
